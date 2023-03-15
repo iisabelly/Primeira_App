9128c6fd6e61140d0c5c8895e29f8536306e2288
Isabelly <isabellyandrades.ifes@gmail.com>
2023-03-15 09:08:10 -0300

    Comentários explicando o código

--- app/src/main/java/andrades/isabelly/primeiraapp/MainActivity.java
+++ app/src/main/java/andrades/isabelly/primeiraapp/MainActivity.java
@@ -14,12 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // encontra o botão por meio da findViewById
         Button btnEnviar = findViewById(R.id.btnEnviar);
+
+        // atribuindo o método a ação OnClick do botão
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
+
+            // pega conteúdo escrito no textView e envia para a NextActivity
             public void onClick(View view) {
+                // por meio do findViewById encontra o textView para pegar o texto escrito para a
+                // NextActivity
                 EditText etDigiteAqui = findViewById(R.id.etDigiteAqui);
                 String textoDigitado = etDigiteAqui.getText().toString();
+
+                // cria a intent para fazer a navegação entre a MainActivity e a NextActivity e
+                // adiciona a ela os valores para "levar" para NextActiviy
                 Intent i = new Intent(MainActivity.this, NextActivity.class);
                 i.putExtra("texto", textoDigitado);
                 startActivity(i);
--- app/src/main/java/andrades/isabelly/primeiraapp/NextActivity.java
+++ app/src/main/java/andrades/isabelly/primeiraapp/NextActivity.java
@@ -8,13 +8,18 @@ import android.widget.TextView;
 
 public class NextActivity extends AppCompatActivity {
 
+    // ações que serão feitas quando NextActivity for criada
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
 
+        // pega conteúdo da intent criada no MainActivity e inclui na NextActivity
         Intent i = getIntent();
         String textoDigitado = i.getStringExtra("texto");
+
+        // por meio do findViewById encontra o textView para a fazer o display do texto que foi
+        // digitado na MainActivity
         TextView tvText = findViewById(R.id.tvText);
         tvText.setText(textoDigitado);
     }
